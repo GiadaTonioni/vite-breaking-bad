5d9598b759355a66e2d66a3f2fbbf52aad7230ef
GiadaTonioni <giada.tonioni@gmail.com>
2023-06-27 11:31:48 +0200

    api non va

--- src/components/AppPokemonList.vue
+++ src/components/AppPokemonList.vue
@@ -1,13 +1,40 @@
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+    data() {
+        return {
+            pokemonList: []
+        }
+    },
+    mounted() {
+        axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons').then((response) => {
+            this.pokemonList = response.data.results;
+        })
+    }
 }
 </script>
 <template lang="">
-    <div>
-        Pokemon
+    <div class="container">
+        <div class="row">
+            <div class="col back">
+                <div class="container">
+                    <div class="row">
+                        <div v-for="(pokemon, index) in pokemonList" :key="pokemon.id" class="col-6 col-md-4">
+                            {{pokemon.name}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     </div>
 </template>
-<style lang="">
-    
+<style lang="scss" scoped>
+.back {
+    background-color: rgb(199, 198, 198);
+    border-radius: 30px;
+    height: 700px;
+    margin-top: 20px;
+}
 </style>
